482bdd0e7c8e24221640c403aa757c3c4822b9c1
yal-babe <lrxya@qq.com>
2020-10-25 14:13:02 +0800

    上传Blog文件  v1.0

---
+++ footer.inc.php
@@ -0,0 +1,23 @@
+<div class="foot">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <a href="https://beian.miit.gov.cn/" target="_blank">桂ICP备2020008270号-1</a>
+            </div>
+            <div class="col-sm-8 fr">Copyright © 2020<a href="#">一杯香梨</a> 版权所有.
+            </div>
+            <p></p>
+        </div>
+        <!-- 弹出层 -->
+        <div class="so">
+            <i class="iconfont" title="关闭">×</i>
+            <div class="container">
+                <form action="https://www.baidu.com/baidu" target="_blank">
+                    <input name="tn" type="hidden" value="SE_zzsearchcode_shhzc78w">
+                    <input type="text" name="word" class="form-control" baiduSug="1" value="百度搜索" />
+                    <input type="submit" style="display: none;" />
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ friends.php
@@ -0,0 +1,97 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Friends - 一杯香梨</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <!-- Js -->
+    <link rel="stylesheet" href="style/css/bootstrap.min.css">
+    <script src="style/js/jquery.min.js"></script>
+    <script src="style/js/tether.min.js"></script>
+    <script src="style/js/bootstrap.min.js"></script>
+    <link href="style/css/style.css" type="text/css" rel="stylesheet" />
+    <link rel="shortcut icon" href="style/images/favicon.ico" type="image/x-icon" />
+</head>
+
+<body>
+    <!-- Header -->
+    <div class="nav">
+        <a class="logo" href="index.html"><img src="style/images/logo.jpg" width="40" title="一杯香梨"></a>
+        <a class="logo pull-right sobtn"><span class="glyphicon glyphicon-search" style="color: #B4B4B4;"></span></a>
+    </div>
+    <div class="container">
+        <div class="links">
+            <div class="linkt">
+                <h3>朋友们</h3>
+                <h5>Friends</h5>
+            </div>
+            <div class="linkb">
+                <ul class="row">
+                    <li class="col-md-4">
+                        <a href="#" target="_blank">
+                            <img src="#" width="30" />
+                            <b>欢</b>
+                            <p>xxx的博客</p>
+                        </a>
+                    </li>
+                    <li class="col-md-4">
+                        <a href="#" target="_blank">
+                            <img src="#" width="30" />
+                            <b>迎</b>
+                            <p>xxx的博客</p>
+                        </a>
+                    </li>
+                    <li class="col-md-4">
+                        <a href="#" target="_blank">
+                            <img src="#" width="30" />
+                            <b>大</b>
+                            <p>xxx的博客</p>
+                        </a>
+                    </li>
+                    <li class="col-md-4">
+                        <a href="#" target="_blank">
+                            <img src="#" width="30" />
+                            <b>家</b>
+                            <p>xxx的博客</p>
+                        </a>
+                    </li>
+                    <li class="col-md-4">
+                        <a href="#" target="_blank">
+                            <img src="#" width="30" />
+                            <b>加</b>
+                            <p>xxx的博客</p>
+                        </a>
+                    </li>
+                    <li class="col-md-4">
+                        <a href="#" target="_blank">
+                            <img src="#" width="30" />
+                            <b>入</b>
+                            <p>xxx的博客</p>
+                        </a>
+                    </li>
+                </ul>
+                <div class="linka">
+                    <h3>申请友链</h3>
+                    <p>本站标题：一杯香梨</p>
+                    <p>本站描述：记录生活</p>
+                    <p>本站链接：https://www.yal.pub</p>
+                    <hr />
+                    <p>申请友情链接之前请先在贵站做好本站的链接，然后通过以上格式发送Email，我将在最短的时间回复。Email：
+                        <a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=lrxya@qq.com" target="_blank">lrxya@qq.com</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <div class="clear"></div>
+    <?php
+    include 'footer.inc.php';
+    ?>
+    <div class="to-top">↑</div>
+    <script src="style/js/GeeYo.js"></script>
+</body>
+
+</html>
